6a1f3140f665ce8676f576223f34e408286ddc2b
Libby Horacek <libby@positiondev.com>
2024-11-10 13:00:30 -0500

    Add Display

---
+++ src/Garden/Display/Model.elm
@@ -0,0 +1,36 @@
+module Garden.Display.Model exposing (..)
+
+
+import Array exposing (Array)
+import Dict
+import Garden.Model exposing (GridName(..), Msg(..), Plant(..))
+import Garden.Grid.Model exposing (Grid, CellState(..))
+
+
+type alias Display =
+   { rows : Int, 
+     columns : Int,
+     plants : Array Plant,
+     grid: Grid } 
+
+
+listDisplay : Display -> List (List (Maybe Plant))
+listDisplay display = 
+  List.foldr (\r rows -> 
+    List.foldr (\c cols -> toDisplayCell display r c :: cols) 
+    [] 
+    (List.range 0 (display.columns - 1))
+      :: rows) 
+    [] 
+    (List.range 0 (display.rows - 1))
+
+getPlant : Display -> Int -> Int -> Plant
+getPlant display row col = 
+    Maybe.withDefault Blue (Array.get (row * col + col) display.plants)
+
+toDisplayCell : Display -> Int -> Int -> Maybe Plant
+toDisplayCell display row col  = 
+  case Dict.get (row, col) display.grid of
+      Just Alive -> Just (getPlant display row col)
+      _ -> Nothing
+
---
+++ tests/Garden/Display/ModelTests.elm
@@ -0,0 +1,90 @@
+module Garden.Display.ModelTests exposing (..)
+
+import Array
+import Dict
+import Expect
+import Garden.Grid.Model exposing (CellState(..), Grid)
+import Garden.Model exposing (Plant(..))
+import Garden.Display.Model exposing (..)
+import List exposing ((::))
+import Test exposing (Test, describe, test)
+
+
+smallGrid : Grid
+smallGrid =
+    Dict.fromList
+        [ ( ( 0, 0 )
+          , Alive
+          )
+        , ( ( 0, 1 )
+          , Dead
+          )
+        , ( ( 1, 0 )
+          , Alive
+          )
+        , ( ( 1, 1 )
+          , Alive
+          )
+        ]
+
+
+
+-- A - A
+-- A - -
+
+
+sparseGrid : Grid
+sparseGrid =
+    Dict.fromList
+        [ ( ( 0, 0 )
+          , Alive
+          )
+        , ( ( 0, 2 )
+          , Alive
+          )
+        , ( ( 1, 0 )
+          , Alive
+          )
+        , ( ( 3000, 301 )
+          , Alive
+          )
+        ]
+
+
+suite : Test
+suite =
+    describe "Game of Life"
+        [ describe "getPlant" 
+            [ test "inside bounds, returns a plant" <|
+              \_ -> 
+                let display = { rows = 2, 
+                                columns = 3, 
+                                grid = smallGrid, 
+                                plants = Array.fromList [Yellow, Purple, Pink, Blue] } in
+                Expect.equal (getPlant display 0 1) Purple,
+             test "outside bounds, returns default plant" <|
+              \_ -> 
+                let display = { rows = 2, 
+                                columns = 3, 
+                                grid = smallGrid, 
+                                plants = Array.fromList [Yellow, Purple, Pink, Pink] } in
+                Expect.equal (getPlant display 3 3) Blue], 
+
+          describe "toPlants"
+            [ test "shows display as rows of plants" <|
+                \_ ->
+                    Expect.equal
+                        (listDisplay { rows = 2, columns = 3, grid = smallGrid, plants = Array.repeat 100 Blue })
+                        [ [ Just Blue, Nothing, Nothing ]
+                        , [ Just Blue, Just Blue, Nothing ]
+                        ]
+            , test "sparse grid" <|
+                \_ ->
+                    Expect.equal
+                        (listDisplay { rows = 3, columns = 3, grid = sparseGrid, plants = Array.repeat 100 Blue} )
+                        [ [ Just Blue, Nothing, Just Blue ]
+                        , [ Just Blue, Nothing, Nothing ]
+                        , [ Nothing, Nothing, Nothing ]
+                        ]
+            ]
+        ]
